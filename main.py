2fe2265d3b6e01b0d20cff94ab8ef18629b27808
atuljalal007 <71332636+atuljalal007@users.noreply.github.com>
2021-06-18 23:49:24 +0530

    done

---
+++ main.py
@@ -0,0 +1,13 @@
+#printing types of some objects
+
+print(type(3.4))
+print(type(3))
+print(type('a'))
+print(type('at is in side bracket'))
+print(type(True))
+print(type(None))
+
+print(int(123.56))
+print(float(123))
+print(str(123))
+print(str(None))
